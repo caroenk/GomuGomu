73288f04caa181f806ba0946974bdd9645ffc4c0
Florantin Bocquet <paradoxiforme@gmail.com>
2017-01-26 10:52:41 +0100

    maj

---
+++ webserver/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char **argv)
+{
+	if(argc > 1 && strcmp(argv[1], "-advice") == 0)
+	{
+		printf("Don't panic !\n");
+		return 42;
+	}
+
+	printf("Need an advice ?\n");
+	return 0;
+}
