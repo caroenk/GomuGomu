d38502d352b84e3c3b42ad9f2f86676b1cfc5cb4
Florantin Bocquet <paradoxiforme@gmail.com>
2017-02-08 14:06:26 +0100

    maj

--- webserver/main.c
+++ webserver/main.c
@@ -9,29 +9,47 @@ int main()
 
 	initialiser_signaux();
 
-	while( 1 ) {
+
+	while(1) {
 
 		socket_client = accept( s, NULL, NULL );
 		if( socket_client == -1 ) {
 			perror("accept");
 			return -1;
 		}
-		sleep(1);
-		const char *message_bienvenue = "Bienvenue sur mon serveur\n";
-		write( socket_client, message_bienvenue, strlen(message_bienvenue));
 
-		char p[1000];
+		int pid = fork();
 
-		while(1){
-
-			int r = read(socket_client, p, 1000);
-
-			if(r != 0) printf("%d\n",r);
-
-			int w = write(socket_client, p, r);
-
-			if(w != 0) printf("%d\n",w);
+		if(pid == -1)
+		{
+			close(socket_client);
+			continue;
 		}
+		else if(pid > 0)
+		{
+			continue;
+		}
+		else if(pid == 0)
+		{
+			sleep(1);
+			const char *message_bienvenue = "Bienvenue sur mon serveur\n";
+			write( socket_client, message_bienvenue, strlen(message_bienvenue));
+
+			char p[1000];
+	
+			while(1){
+
+				int r = read(socket_client, p, 1000);
+
+				if(r != 0) printf("%d\n",r);
+	
+				int w = write(socket_client, p, r);
+
+				if(w != 0) printf("%d\n",w);
+			}
+		}
+
+
 	}
 	return 0;
 }
